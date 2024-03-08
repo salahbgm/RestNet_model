f66f8fe3883413f48e0acd1912c984293c982ca0
salahbgm <116580792+salahbgm@users.noreply.github.com>
2024-03-08 11:14:44 +0100

    Add files via upload

---
+++ model.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+
+class block(nn.Module):
+    def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
+        super(block, self).__init__()
+        self.expansion = 4
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv3 = nn.Conv2d(out_channels, out_channels*self.expansion, kernel_size=1, stride=1, padding=0) # notice the output channels here is multiplied by 4 to get the desired output channels
+        self.bn3 = nn.BatchNorm2d(out_channels*self.expansion)
+        self.relu = nn.ReLU()
+        self.identity_downsample = identity_downsample
+        #self.stride = stride
+
+    def forward(self, x):
+        identity = x.clone()
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+
+        if self.identity_downsample is not None:
+            identity = self.identity_downsample(identity)
+
+        x += identity
+        x = self.relu(x)
+        return x
+    
+class ResNet(nn.Module): # [3, 4, 6, 3]
+    def __init__(self, block, layers, image_channels, num_classes):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # Essentially, we are creating the layers of the ResNet model here
+        self.layer1 = self._make_layer(block, layers[0], out_channels=64, stride=1)
+        self.layer2 = self._make_layer(block, layers[1], out_channels=128, stride=2)
+        self.layer3 = self._make_layer(block, layers[2], out_channels=256, stride=2)
+        self.layer4 = self._make_layer(block, layers[3], out_channels=512, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Linear(512*4, num_classes)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc(x)
+
+        return x
+
+    def _make_layer(self, block, num_residual_blocks, out_channels, stride):
+        identity_downsample = None
+        layers = []
+
+        if stride != 1 or self.in_channels != out_channels * 4:
+            identity_downsample = nn.Sequential(nn.Conv2d(self.in_channels, out_channels*4, kernel_size=1, stride=stride),
+                                                nn.BatchNorm2d(out_channels*4))
+        layers.append(block(self.in_channels, out_channels, identity_downsample, stride))
+        self.in_channels = out_channels*4
+
+        for i in range(num_residual_blocks - 1):
+            layers.append(block(self.in_channels, out_channels)) # 256 -> 64, 64*4 (256) againb because we are downsampling the input by 2 and the output channels are 4 times the input channels in the block class
+
+        return nn.Sequential(*layers) # * is used to unpack the list of layers and pass it as a sequence of arguments to the Sequential function 
+
+    
+def resnet50(img_channels=3, num_classes=1000):
+    return ResNet(block, [3, 4, 6, 3], img_channels, num_classes)
+
+# def resnet101(img_channels=3, num_classes=1000):
+#     return ResNet(block, [3, 4, 23, 3], img_channels, num_classes)
+
+# def resnet152(img_channels=3, num_classes=1000):
+#     return ResNet(block, [3, 8, 36, 3], img_channels, num_classes)
+
+def test():
+    net = resnet50()
+    x = torch.randn(2, 3, 224, 224)
+    y = net(x).to('cuda')
+    print(y.shape)
+
+test()
